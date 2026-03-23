5bed2d8bb9b0cd7b42f2b61ad490e4812559d5fd
suhwan <jswwwwww@naver.com>
2026-03-23 10:15:29 +0900

    chore: RiftMind 프로젝트 초기 설정

---
+++ summoner-service/src/main/java/com/riftmind/summoner/SummonerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.riftmind.summoner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummonerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummonerServiceApplication.class, args);
+	}
+
+}
---
+++ summoner-service/src/test/java/com/riftmind/summoner/SummonerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riftmind.summoner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummonerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
